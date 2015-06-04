7a0c24dc1a8c38744b17806b9847d01e25317c1a
vaibhav valecha <vaibatch2001@gmail.com>
2015-06-05 01:21:19 +0530

    Updated

--- plot4.R
+++ plot4.R
@@ -7,10 +7,14 @@ DateTime <- paste(input2$Date,input2$Time)
 DateTime <- strptime(DateTime, format = "%d/%m/%Y %H:%M:%S")
 input2$DateTime <- DateTime 
 
-## Generate the plot and export to PNG
-png('plot3.png')
+## Generate the plots and export to PNG
+png('plot4.png')
+par(mfrow=c(2,2))
+hist(input2$Global_active_power, col = "Red", main = "", xlab = "Global Active Power (kilowatts)")
+plot(input2$DateTime, input2$Global_active_power, type = "l", xlab = "datetime", ylab = "Global Active Power (kilowatts)")
 plot(input2$DateTime,input2$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(input2$DateTime,input2$Sub_metering_2,col="red")
 lines(input2$DateTime,input2$Sub_metering_3,col="blue")
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"), bty = "n")
+plot(input2$DateTime, input2$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 dev.off()
